83dbdbcc69d58316e02871fc292ab1a7cc0f38bd
baeklee <77774331+monjk557@users.noreply.github.com>
2023-01-16 22:35:07 +0900

    feat: route 생성한 3개 API 컨트롤러 생성

---
+++ src/app/Notification/notificationController.js
@@ -0,0 +1,14 @@
+const notificationProvider = require("./notificationProvider")
+const notificationService = require("./notificationService")
+
+exports.postNotifications = async(req,res)=>{
+
+};
+
+exports.getNotifications = async(req,res)=>{
+
+};
+
+exports.getNotificationsStatus = async(req,res)=>{
+
+}
