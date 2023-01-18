2c0f4d2f0791ec85e74f864055b3fb1a7cfcc0fd
Jang-DongHo <donghoo18@naver.com>
2023-01-18 15:50:11 +0900

    [FEAT #7] 카카오 로그인 API 구현

--- config/response.js
+++ config/response.js
@@ -1,18 +1,19 @@
-const response = ({isSuccess, code, message}, result) => {
-   return {
-        isSuccess: isSuccess,
-        code: code,
-        message: message,
-        result: result
-   }
+const response = ({ isSuccess, code, message }, result) => {
+  return {
+    isSuccess: isSuccess,
+    code: code,
+    message: message,
+    result: result,
   };
+};
 
-  const errResponse = ({isSuccess, code, message}) => {
-    return {
-        isSuccess: isSuccess,
-        code: code,
-        message: message
-      }
+const errResponse = ({ isSuccess, code, message }, detailMessage) => {
+  return {
+    isSuccess: isSuccess,
+    code: code,
+    message: message,
+    detailMessage: detailMessage,
   };
-  
-  module.exports = { response, errResponse };
+};
+
+module.exports = { response, errResponse };
--- src/app/User/userService.js
+++ src/app/User/userService.js
@@ -69,3 +69,50 @@ exports.oauthAppleLogin = async function (authorizationCode) {
     return errResponse(baseResponse.SOCIAL_LOGIN_SERVER_ERROR);
   }
 };
+
+exports.oauthKakaoLogin = async function (authorizationCode) {
+  try {
+    const accessToken = authorizationCode;
+    const idToken = jwt.decode(accessToken.id_token);
+    const { email, sub } = idToken;
+
+    // oauthId로 유저 조회
+    const selectUserOauthIdParams = [1, sub];
+    let userRow = await userProvider.oauthIdCheck(selectUserOauthIdParams);
+
+    // user가 조회되지 않으면, 계정 생성
+    if (!userRow) {
+      const rememberMeToken = uuid.v6(); // uuid-with-v6 사용해서 rememberMeToken 생성
+      const insertUserInfoParams = [email, 1, sub, rememberMeToken];
+
+      const connection = await pool.getConnection(async (conn) => conn);
+      try {
+        await connection.beginTransaction();
+        await userDao.insertUserInfo(connection, insertUserInfoParams);
+        await connection.commit();
+      } catch (err) {
+        logger.error(`App - createUser Service error\n: ${err.message}`);
+        return errResponse(baseResponse.DB_ERROR);
+      } finally {
+        connection.release();
+      }
+    }
+
+    userRow = await userProvider.oauthIdCheck(selectUserOauthIdParams); // 다시 user 조회 (bad smell)
+    const userIdx = userRow[0].idx;
+    // jwt 생성
+    const token = createJwt({
+      userIdx,
+    });
+    const { rememberMeToken } = userRow[0];
+
+    return response(baseResponse.SUCCESS, {
+      userIdx,
+      jwt: token,
+      rememberMeToken,
+    });
+  } catch (err) {
+    logger.error(`App - oauthAppleLogin Service error\n: ${err.message}`);
+    return errResponse(baseResponse.SOCIAL_LOGIN_SERVER_ERROR);
+  }
+};
