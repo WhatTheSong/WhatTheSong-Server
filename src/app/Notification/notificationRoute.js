8680017d02c726794aa37205f423bc6082d93667
baeklee <77774331+monjk557@users.noreply.github.com>
2023-01-31 21:36:07 +0900

    feat: 파일 업로드 api 생성

--- src/app/Notification/notificationRoute.js
+++ src/app/Notification/notificationRoute.js
@@ -7,14 +7,14 @@ module.exports = (app) => {
     app.get('/app/notification', notification.testNotifications);
 
     // 1-1. 댓글 알림 생성
-    app.post('app/notification/reply', notification.postReplyNotifications);
+    app.post('/app/notification/reply', notification.postReplyNotifications);
 
     // 1-2. 공감 알림 생성
-    app.post('app/notification/like', notification.postLikeNotifications);
+    // app.post('/app/notification/like', notification.postLikeNotifications);
 
     // 2. 알림조회
-    app.get('/app/notification/:id', notification.getNotifications);
+    // app.get('/app/notification/:id', notification.getNotifications);
 
     // 3. 알림 상태조회
-    app.get('/app/notification/status/:id', notification.getNotificationsStatus);
+    // app.get('/app/notification/status/:id', notification.getNotificationsStatus);
 };
