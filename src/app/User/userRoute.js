debf62d34b5195f7b3e2f85e0e1f4b47757cd01f
Jang-DongHo <donghoo18@naver.com>
2023-02-03 12:28:33 +0900

    [Remove] 필요없는 코드 삭제

--- src/app/User/userRoute.js
+++ src/app/User/userRoute.js
@@ -3,7 +3,7 @@ module.exports = function (app) {
   const jwtMiddleware = require("../../../config/jwtMiddleware");
 
   // 애플 소셜 로그인 API (JWT 발급)
-  app.post("/app/users/oauth/apple", user.oauthAppleLogin);
+  //app.post("/app/users/oauth/apple", user.oauthAppleLogin);
 
   // 카카오 소셜 로그인 API (JWT 발급)
   app.post("/app/users/oauth/kakao", user.oauthKakaoLogin);
