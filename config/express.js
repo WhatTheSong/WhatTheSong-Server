42c3ee2f2cdf0f8298e915b8591f30e5c71b208c
Jang-DongHo <donghoo18@naver.com>
2023-02-04 22:07:14 +0900

    [Feat #23] 노래 질문 게시판 조회 API 중간저장

--- config/express.js
+++ config/express.js
@@ -18,11 +18,8 @@ module.exports = function () {
   // TODO: 도메인을 추가할 경우 이곳에 Route를 추가하세요.
   require("../src/app/User/userRoute")(app);
   require("../src/app/Notification/notificationRoute")(app);
+  require("../src/app/User/userRoute")(app);
+  require("../src/app/Board/boardRoute")(app);
 
-    /* App (Android, iOS) */
-    // TODO: 도메인을 추가할 경우 이곳에 Route를 추가하세요.
-    require('../src/app/User/userRoute')(app);
-    require('../src/app/Board/boardRoute')(app);
-
-    return app;
+  return app;
 };
--- src/app/Board/boardRoute.js
+++ src/app/Board/boardRoute.js
@@ -1,35 +1,38 @@
-module.exports = function(app){
-    const board = require('./boardController');
-    const jwtMiddleware = require('../../../config/jwtMiddleware');
+module.exports = function (app) {
+  const board = require("./boardController");
+  const jwtMiddleware = require("../../../config/jwtMiddleware");
 
-    // 전체 노래 추천 게시글 조회 API
-    app.get("/app/boards/recommendations", board.getRecommendations);
+  // 전체 노래 추천 게시글 조회 API
+  app.get("/app/boards/recommendations", board.getRecommendations);
 
-    // 추천 게시글 작성 API
-    app.post(
-        "/app/boards/:userIdx/recommendations/:recommendationIdx",
-        jwtMiddleware,
-        board.postRecommendation
-    );
+  // 추천 게시글 작성 API
+  app.post(
+    "/app/boards/:userIdx/recommendations/:recommendationIdx",
+    jwtMiddleware,
+    board.postRecommendation
+  );
 
-    // 추천 게시글 상세 조회 API
-    app.get(
-        "/app/boards/:userIdx/recommendatons/:recommendationIdx",
-        jwtMiddleware,
-        board.getRecommendation
-    );
+  // 추천 게시글 상세 조회 API
+  app.get(
+    "/app/boards/:userIdx/recommendatons/:recommendationIdx",
+    jwtMiddleware,
+    board.getRecommendation
+  );
 
-    // 추천 게시글 삭제 API
-    app.delete(
-        "/app/boards/:userIdx/recommendatons/:recommendationIdx",
-        jwtMiddleware,
-        board.deleteRecommendation
-    );
+  // 추천 게시글 삭제 API
+  app.delete(
+    "/app/boards/:userIdx/recommendatons/:recommendationIdx",
+    jwtMiddleware,
+    board.deleteRecommendation
+  );
 
-    // 추천 게시글 수정 API
-    app.patch(
-        "/app/boards/:userIdx/recommendatons/:recommendationIdx",
-        jwtMiddleware,
-        board.patchRecommendation
-    );
+  // 추천 게시글 수정 API
+  app.patch(
+    "/app/boards/:userIdx/recommendatons/:recommendationIdx",
+    jwtMiddleware,
+    board.patchRecommendation
+  );
+
+  // 게시판 조회 API
+  app.get("/app/boards/questions", board.getQuestionPosts);
 };
