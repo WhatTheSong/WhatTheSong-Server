395d553766e640ea3853f2ba8e7bae59dc5e15c6
baeklee <77774331+monjk557@users.noreply.github.com>
2023-02-07 18:38:13 +0900

    feat: 기능 리팩토링 및 완성
    S3 업로드를 promise 객체생성해서 콜백을 promise로 처리, geturl 없이 업로드하면 바로 오브젝트 url
    얻음
    얻은 url dao로 rds에 insert

--- src/app/Storage/storageService.js
+++ src/app/Storage/storageService.js
@@ -1,35 +1,66 @@
 const baseResponse = require("../../../config/baseResponseStatus");
 const { response, errResponse } = require("../../../config/response");
+const storageDao = require("./storageDao");
 const AWS = require('aws-sdk');
+const {pool} = require("../../../config/database");
+const userDao = require("../User/userDao");
+const {logger} = require("../../../config/winston");
+
 exports.uploadFileToS3 = async (uploadFile)=>{
+
+    // AWS 설정
     AWS.config.update({
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_KEY,
         region: process.env.AWS_REGION
     });
-    const s3 = new AWS.S3();
+
+    // 업로드파일을 base64데이터로 변환
     const base64data = new  Buffer.alloc(uploadFile.size, uploadFile,'binary');
-    const params = {
-        Bucket: process.env.AWS_BUCKET_NAME,
-        Key: uploadFile.name,
-        Body: base64data
+
+    // S3업로드를 위한 객체생성
+    const s3 = new AWS.S3();
+
+    // 업로드후 url 전달받기 위한 promise 객체 생성 메소드
+    const uploadToS3 = (file) => {
+        return new Promise((resolve, reject) => {
+            const params = {
+                Bucket: process.env.AWS_BUCKET_NAME,
+                Key: uploadFile.name,
+                Body: file
+            };
+
+            s3.upload(params, (err, data) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(data.Location);
+                }
+            });
+        });
     };
-
-    s3.upload(params, (err, data) =>{
-        if (err) {
-            console.log(`Error uploading file ${uploadFile.name}.`, err);
-            return false
-
-        }
-        if (data) {
-            console.log(`File uploaded successfully. ${data.Location}`);
-            return true
-        }
-    });
+    return await uploadToS3(base64data);
 }
 
-exports.postS3URL = async (url) =>{
-
-}
+exports.postS3URL = async (postId,url) =>{
+    const connection = await pool.getConnection(async (conn) => conn);
+    try {
+        await connection.beginTransaction();
+        const postS3URLParams = [postId, url];
+        await storageDao.insertS3URL(
+            connection,
+            postS3URLParams
+        );
+        await connection.commit();
+    }catch (err){
+        connection.rollback();
+        logger.error(
+            `App - postS3URL Service error\n: ${err.message}`
+        );
+        throw new Error(baseResponse.DB_ERROR);
+    } finally {
+        connection.release();
+    }
+};
 
 
