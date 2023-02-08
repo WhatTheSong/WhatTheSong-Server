c97876fc9615f0f9cf1759225e725873e5af7884
baeklee <77774331+monjk557@users.noreply.github.com>
2023-02-08 16:05:31 +0900

    docs: 오타 수정

--- src/app/Storage/storageRoute.js
+++ src/app/Storage/storageRoute.js
@@ -3,8 +3,8 @@ module.exports = (app) => {
     const storage = require("./storageController");
 
     // 음성 업로드
-    app.post("/app/storage/voice", storage.postVoiceToS3);
+    app.post("/app/storages/voice", storage.postVoiceToS3);
 
     // 파일 경로 조회
-    app.get("/app/storage/voice/:boardIdx", storage.getVoiceByIdx);
+    app.get("/app/storages/voice/:boardIdx", storage.getVoiceByIdx);
 }
