10c99cb507ac5f2c76e3a78fd59612fff2b1cc0d
baeklee <77774331+monjk557@users.noreply.github.com>
2023-02-12 18:23:38 +0900

    feat: url 변경 dao 추가

--- src/app/Storage/storageDao.js
+++ src/app/Storage/storageDao.js
@@ -1,4 +1,5 @@
 // S3 url 저장 row 생성
+
 const insertS3URL = async (connection, postS3URLParams)=>{
     const insertS3UrlQuery = `INSERT INTO Storage(boardIdx, url) VALUES (?,?);`;
     await connection.query(insertS3UrlQuery, postS3URLParams)
@@ -10,7 +11,13 @@ const selectS3URLByPostId = async (connection, boardIdx)=>{
     return await connection.query(selectS3URLByPostIdQuery, boardIdx);
 }
 
+const updateS3URL = async (connection, updateS3URLParams)=>{
+    const updateS3UrlQuery = `UPDATE Storage SET url = ? WHERE boardIdx = ? ;`;
+    return await connection.query(updateS3UrlQuery, updateS3URLParams);
+}
+
 module.exports ={
     insertS3URL,
-    selectS3URLByPostId
+    selectS3URLByPostId,
+    updateS3URL
 }
