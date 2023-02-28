abc0ec52f6205c98a6dfc0eec5f108b6c45eb200
baeklee <77774331+monjk557@users.noreply.github.com>
2023-02-28 22:06:08 +0900

    feat: kafka 모듈 파라미터 수정

--- config/kafka.js
+++ config/kafka.js
@@ -1,65 +1,21 @@
-const { Kafka, CompressionTypes, logLevel } = require('kafkajs')
+const { Kafka } = require('kafkajs')
+require("dotenv").config();
 
-
-
-module.createLikeNotification= async (sender,receiverToken) =>{
-    const host = process.env.HOST_IP
-
+const kafkaProducer = (sender,message,receiver)=>{
     const kafka = new Kafka({
-        logLevel: logLevel.DEBUG,
-        brokers: [`${host}:9092`],
-        clientId: 'notification-producer',
+        brokers: [`${process.env.HOST_IP}:9092`],
     })
-
-    const topic = process.env.KAFKA_TOPICS
     const producer = kafka.producer()
-    const msg = {
-        sender:sender,
-        receiverToken:receiverToken,
-    }
-    const sendMessage = (msg) => {
-        return producer
-            .send({
-                topic,
-                compression: CompressionTypes.GZIP,
-                messages: [msg]
-            })
-            .then(console.log)
-            .catch(e => console.error(`[notification/producer] ${e.message}`, e))
-    }
-
-
-    const errorTypes = ['unhandledRejection', 'uncaughtException']
-    const signalTraps = ['SIGTERM', 'SIGINT', 'SIGUSR2']
-
-    errorTypes.forEach(type => {
-        process.on(type, async () => {
-            try {
-                console.log(`process.on ${type}`)
-                await producer.disconnect()
-                process.exit(0)
-            } catch (_) {
-                process.exit(1)
-            }
-        })
-    })
-
-    signalTraps.forEach(type => {
-        process.once(type, async () => {
-            try {
-                await producer.disconnect()
-            } finally {
-                process.kill(process.pid, type)
-            }
-        })
-    })
-
     const run = async () => {
         await producer.connect()
-        setInterval(sendMessage, 3000)
+        await producer.send({
+            topic: process.env.KAFKA_TOPICS_TEST,
+            messages:message,
+        })
     }
-
-    run().catch(e => console.error(`[example/producer] ${e.message}`, e))
-
-
+    run().catch(e => {
+        console.error(`[kafka/producer] ${e.message}`, e)
+    })
 }
+
+module.exports = kafkaProducer;
